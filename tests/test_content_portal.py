ea4f203cd10dcbf9a59f7396514da5f6290d8fdc
vepry <vepry.p@gmail.com>
2022-02-22 12:34:44 +0700

    content

--- tests/test_content_portal.py
+++ tests/test_content_portal.py
@@ -6,4 +6,4 @@ from arcgisenterprisemigration.cli import portal_content
 class TestPortalContentDump(unittest.TestCase):
     def test_load_to_console(self):
         runner = CliRunner()
-        result = runner.invoke(portal_content.load_cmd_portal_content, ['https://peta.gakkum.menlhk.go.id/portal', 'portaladmin', 'Passw0rd1234'])
+        result = runner.invoke(portal_content.load_cmd_portal_content, ['', '', ''])
--- tests/test_user.py
+++ tests/test_user.py
@@ -7,5 +7,5 @@ from arcgisenterprisemigration.cli import users_roles
 class TestUserDumpToConsole(unittest.TestCase):
     def test_load_to_console(self):
         runner = CliRunner()
-        result = runner.invoke(users_roles.load_cmd_users_roles, ['https://peta.gakkum.menlhk.go.id/portal', 'portaladmin', 'Passw0rd1234'])
+        result = runner.invoke(users_roles.load_cmd_users_roles, ['', '', ''])
         # assert '' in ''
