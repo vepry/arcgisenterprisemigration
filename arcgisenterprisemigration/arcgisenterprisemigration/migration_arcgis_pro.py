9fb2a02c8a542d2b3b9c98a22002d239a206b023
vepry <vepry.p@gmail.com>
2022-05-09 10:17:03 +0700

    update

--- arcgisenterprisemigration/arcgisenterprisemigration/migration_arcgis_pro.py
+++ arcgisenterprisemigration/arcgisenterprisemigration/migration_arcgis_pro.py
@@ -8,30 +8,37 @@ class AuthArcgisPortalPro():
         self.portal_url = url_portal
         self.auth = arcpy.SignInToPortal(url_portal, username_portal, password_portal)
 
+
 class DeployArcgisPortalPro():
-    def __init__(self, auth: AuthArcgisPortalPro, 
-    aprx_path) -> None:
+    def __init__(self, auth: AuthArcgisPortalPro,
+                 aprx_path) -> None:
         self._config = ConfigCMD()
         self._auth = auth
         self._aprx = arcpy.mp.ArcGISProject(aprx_path)
 
-    def deploy_service(self, svc_name, portal_folder, server_folder, lyr_mxd_path, copy_data_to_server = False, overwrite_service=False,
-    credits='',summary='', tags='', description='', activate_feature_service=False):
+    def deploy_service(self, svc_name, portal_folder, server_folder, lyr_mxd_path, copy_data_to_server=False,
+                       overwrite_service=False,
+                       credits='', summary='', tags='', description='', activate_feature_service=False):
         self.append_layer(lyr_mxd_path)
         self._prepare_map_service_portal_sd(svc_name, self._auth.portal_url, portal_folder, server_folder,
-            copy_data_to_server, overwrite_service,
-            credits,summary, tags, description, activate_feature_service)
-        
+                                            copy_data_to_server, overwrite_service,
+                                            credits, summary, tags, description, activate_feature_service)
 
     def append_layer(self, layer_mxd_path: str):
         self._aprx.importDocument(layer_mxd_path)
 
-    def _prepare_map_service_portal_sd(self, svc_name, 
-    portal_url, portal_folder, server_folder,
-    copy_data_to_server = False, overwrite_service=False,
-    credits='',summary='', tags='', description='', activate_feature_service=False):
+    def _prapare_aprx(self, path_template_aprx, file_path_lyr):
+        proj = arcpy.mp.ArcGISProject(path_template_aprx)
+        proj.saveACopy(self._config.template_deploy_path)
+        self._aprx = arcpy.mp.ArcGISProject(self._config.template_deploy_path)
+
+
+    def _prepare_map_service_portal_sd(self, svc_name,
+                                       portal_url, portal_folder, server_folder,
+                                       copy_data_to_server=False, overwrite_service=False,
+                                       credits='', summary='', tags='', description='', activate_feature_service=False):
         m = self._aprx.listMaps('*')[0]
-        s_draft = arcpy.sharing.CreateSharingDraft('FEDERATED_SERVER','MAP_SERVICE', svc_name, m)
+        s_draft = arcpy.sharing.CreateSharingDraft('FEDERATED_SERVER', 'MAP_SERVICE', svc_name, m)
         s_draft.copyDataToServer = copy_data_to_server
         s_draft.overwriteExistingService = overwrite_service
         s_draft.portalFolder = portal_folder
@@ -44,12 +51,13 @@ class DeployArcgisPortalPro():
         s_draft.description = description
 
         s_draft.exportToSDDraft(self._config.deploy_sd_folder + svc_name + '.sddraft')
-        out_file = self.activate_some_features(svc_name, self._config.deploy_sd_folder + svc_name + '.sddraft', activate_feature_service)
+        out_file = self.activate_some_features(svc_name, self._config.deploy_sd_folder + svc_name + '.sddraft',
+                                               activate_feature_service)
         sd_out_file = self._prepare_sd_file(svc_name, out_file)
         return sd_out_file
 
-    def activate_some_features(self, svc_name, sddraft_path, 
-    active_feature = False):
+    def activate_some_features(self, svc_name, sddraft_path,
+                               active_feature=False):
         doc = DOM.parse(sddraft_path)
         typeNames = doc.getElementsByTagName('TypeName')
         for typeName in typeNames:
@@ -76,13 +84,7 @@ class DeployArcgisPortalPro():
         f.close()
         return outFile
 
-
-
-
     def _prepare_sd_file(self, svc_name, sd_draft_file_path):
         outfile = self._config.deploy_sd_folder + svc_name + '.sd'
         arcpy.StageService_server(sd_draft_file_path, outfile)
         return outfile
-
-
-        
